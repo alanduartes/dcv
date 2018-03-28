fd5feaa7f6d518c5a8aeb34364396a5defab8d7b
Alan Duarte dos Santos <alanduartes@gmail.com>
2018-03-27 22:14:48 -0300

    Adiciona os componentes: courses/experience/knowledge/language/school

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Alan Duarte dos Santos'
-  email = 'alanduartes@gmail.com'
-  fone = '(41) 9 9667-3259'
+  name = 'Alan Duarte dos Santos';
+  email = 'alanduartes@gmail.com';
+  fone = '(41) 9 9667-3259';
 
-  toggle(info) {
-    
-    if (info == 'E') { // Education
-      alert('ED');
-    } else if (info == 'X') { // Experience
-      alert('EX');
-    } else if (info == 'C') { // Courses
-      alert('C');
-    } else if (info == 'K') { // Knowledge
-      alert('K');
-    } else if (info == 'L') { // Language
-      alert('L');
+  // The description window is opened?
+  description_open = true;
+  // Which menu item is selected?
+  selected = '';
+  // To show description (transform)
+  style = {};
+
+  toggleDescription(selected = '') {
+    let opened = <HTMLElement> document.getElementsByClassName('opened')[0];
+
+    this.selected = selected;
+
+    if(this.description_open == true) {
+
+      this.style = {
+        "transform": "translate(0,0)"
+      };
+
+      this.description_open = false;
+
+    } else {
+
+      this.style = {
+        "transform": "translate(0,100%)"
+      };
+
+      this.selected = '';
+
+      this.description_open = true;
+
     }
+
+    return this.style;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
+import { SchoolComponent } from './school/school.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { CoursesComponent } from './courses/courses.component';
+import { KnowledgeComponent } from './knowledge/knowledge.component';
+import { LanguageComponent } from './language/language.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SchoolComponent,
+    ExperienceComponent,
+    CoursesComponent,
+    KnowledgeComponent,
+    LanguageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Angular2FontawesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
