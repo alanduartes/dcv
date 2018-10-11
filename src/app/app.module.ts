0eb378d54a913bda2924341bc1666294ac89a2b3
teste <teste@2ttes.com>
2018-10-11 15:52:51 -0300

    Remoção de componentes que não serão utilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
-import { SchoolComponent } from './school/school.component';
-import { ExperienceComponent } from './experience/experience.component';
-import { CoursesComponent } from './courses/courses.component';
-import { KnowledgeComponent } from './knowledge/knowledge.component';
-import { LanguageComponent } from './language/language.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SchoolComponent,
-    ExperienceComponent,
-    CoursesComponent,
-    KnowledgeComponent,
-    LanguageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
