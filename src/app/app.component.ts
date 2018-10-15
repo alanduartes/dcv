73c216dc4d051dd444afea1b233cc513d372fcee
teste <teste@2ttes.com>
2018-10-15 16:41:53 -0300

    Ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,38 +11,4 @@ export class AppComponent {
   email = 'alanduartes@gmail.com';
   fone = '(41) 9 9667-3259';
 
-  // The description window is opened?
-  description_open = true;
-  // Which menu item is selected?
-  selected = '';
-  // To show description (transform)
-  style = {};
-
-  toggleDescription(selected = '') {
-    let opened = <HTMLElement> document.getElementsByClassName('opened')[0];
-
-    this.selected = selected;
-
-    if(this.description_open == true) {
-
-      this.style = {
-        "transform": "translate(0,0)"
-      };
-
-      this.description_open = false;
-
-    } else {
-
-      this.style = {
-        "transform": "translate(0,100%)"
-      };
-
-      this.selected = '';
-
-      this.description_open = true;
-
-    }
-
-    return this.style;
-  }
 }
